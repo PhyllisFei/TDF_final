96f1d3cea7f60de6df9be89dc49a7e553b8b44ff
Phyllis <zf9702@gmail.com>
2022-11-26 23:53:24 -0800

    add popup window style

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+let cvs;
+let bgclr;
+
 let members = [];
 let m;
 let mImgs = [];
+
+let popupDiv;
+let text;
+let btn1;
+let btn2;
 
 //preload image to array
 function preload() {
@@ -10,7 +18,8 @@ function preload() {
 }
 
 function setup() {
-    createCanvas(600, 400);
+    cvs = createCanvas(windowWidth, windowHeight);
+    bgclr = color(200);
 
     for (let i = 0; i < 5; i++) {
         let x = 40 + 100 * i;
@@ -19,25 +28,70 @@ function setup() {
         m = new Member(x, y, r);
         members.push(m);
     }
+
+    popupWindow();
 }
 
 function draw() {
-    background(255);
+    background(bgclr);
 
     for (m of members) {
-        //glowing effect of the note sender with mouse hover: micic the visual effect when a note is sent
+        // for (let i = 0; i < members.length; i++) {
+        console.log(members.length);
+        //mouse hover: glowing effect
         if (m.contains(mouseX, mouseY)) {
             m.changeGlow(color(0, 0, 0, 150), 15);
-            //***** add attraction-collision behavior between sender and receiver: sender approaches to receiver — bounce — and collide *****//
-            // m.attract()
-            // m.collide();
+
+            //show popup window
+            if (mouseIsPressed) {
+                // m.popupWindow();
+            }
         } else {
             m.changeGlow(0, 0);
         }
-        m.move();
+
+        //***** add attraction-collision behavior between sender and receiver: sender approaches to receiver — bounce — and collide *****//
+        // m.attract()
+        // m.collide();
+
+        m.update();
         m.bounce();
         m.display();
     }
+}
+
+function popupWindow() {
+    //create a modal box
+    popupDiv = createDiv();
+    popupDiv.id('popupDiv');
+    popupDiv.position(500, 500);
+
+    text = createP('Want to send a note?');
+    text.parent(popupDiv);
+
+    btn1 = createButton('Yes');
+    btn1.class('btn');
+    btn1.id('btn1');
+    btn1.parent(popupDiv);
+    btn1.mousePressed(sendNote);
+
+    btn2 = createButton('No ');
+    btn2.class('btn');
+    btn2.id('btn2');
+    btn2.parent(popupDiv);
+    btn2.mousePressed(hideDiv);
+}
+
+//send note pic if "Yes", currently changes bgclr as a place holder
+function sendNote() {
+    //add MQTT & webSocket listener
+    bgclr = color(100, 230, 180);
+}
+
+//close popup window if "No"
+function hideDiv() {
+    bgclr = color(200);
+    popupDiv.hide();
 }
 
 
@@ -64,26 +118,29 @@ function doubleClicked() {
 
 class Member {
     constructor(x, y, dia) {
-        this.x = x;
-        this.y = y;
-        this.dia = dia;
-        this.xSpd = random(-0.15, 0.15);
-        this.ySpd = random(-0.15, 0.15);
+        // this.x = x;
+        // this.y = y;
+        // this.dia = dia;
+        // this.xSpd = random(-0.15, 0.15);
+        // this.ySpd = random(-0.15, 0.15);
         this.r = random(100);
         this.g = random(255);
         this.b = random(255);
         this.mImg = random(mImgs);
         this.blurriness = 0;
         this.glowColor = 0;
-    }
 
-    changeGlow(clr, blur) {
-        this.blurriness = blur;
-        this.glowColor = clr;
+        this.pos = createVector(x, y);
+        // this.vel = p5.Vector.random2D();
+        this.vel = createVector();
+        this.acc = createVector();
+        // this.mass = m;
+        this.dia = dia;
     }
 
     contains(px, py) {
-        let d = dist(px, py, this.x, this.y)
+        // let d = dist(px, py, this.x, this.y)
+        let d = dist(px, py, this.pos.x, this.pos.y)
         // console.log(d);
         if (d < this.r) {
             console.log('note sent, members interacted!');
@@ -93,9 +150,67 @@ class Member {
         }
     }
 
-    move() {
-        this.x += this.xSpd;
-        this.y += this.ySpd;
+    // popupWindow() {
+    //     //create a modal box
+    //     popupDiv = createDiv();
+    //     popupDiv.id('popupDiv');
+    //     popupDiv.position(this.pos.x, this.pos.y);
+
+    //     text = createP('Want to send a note?');
+    //     text.parent(popupDiv);
+
+    //     btn1 = createButton('Yes');
+    //     btn1.class('btn');
+    //     btn1.id('btn1');
+    //     btn1.parent(popupDiv);
+    //     btn1.mousePressed(sendNote);
+
+    //     btn2 = createButton('No ');
+    //     btn2.class('btn');
+    //     btn2.id('btn2');
+    //     btn2.parent(popupDiv);
+    //     btn2.mousePressed(hideDiv);
+    // }
+
+    changeGlow(clr, blur) {
+        this.blurriness = blur;
+        this.glowColor = clr;
+    }
+
+
+    applyForce(force) {
+        var f = createVector();
+        f = force.copy();
+        f.div(this.mass);
+        this.acc.add(f);
+    }
+
+    attract() {
+        //
+    }
+
+    checkCollisiion(other) {
+        let vector = p5.Vector.sub(other.pos, this.pos);
+        let distanceSq = vector.magSq();
+
+        if (distanceSq < (this.dia + other.dia) * (this.dia + other.dia)) {
+            vector.mult(-0.5);
+            this.applyForce(vector);
+        }
+    }
+
+    applyRestitution(amount) {
+        let value = 1.0 + amount;
+        this.vel.mult(value);
+    }
+
+    update() {
+        // this.x += this.xSpd;
+        // this.y += this.ySpd;
+
+        this.vel.add(this.acc);
+        this.pos.add(this.vel);
+        this.acc.mult(0);
     }
 
     bounce() {
@@ -107,13 +222,7 @@ class Member {
         }
     }
 
-    attract() {
-        //
-    }
 
-    collide() {
-        //
-    }
 
     display() {
         drawingContext.shadowBlur = this.blurriness;
@@ -121,9 +230,9 @@ class Member {
 
         stroke(this.r, this.g, this.b, 100)
         fill(this.r, this.g, this.b, 100);
-        ellipse(this.x, this.y, this.dia, this.dia);
+        ellipse(this.pos.x, this.pos.y, this.dia, this.dia);
 
-        // image( this.mImg, this.x, this.y, this.dia*2, this.dia*2);
-        image(this.mImg, this.x, this.y, this.dia * 2, this.dia * 2);
+        // image(this.mImg, this.x, this.y, this.dia*2, this.dia*2);
+        image(this.mImg, this.pos.x, this.pos.y, this.dia * 2, this.dia * 2);
     }
 }
